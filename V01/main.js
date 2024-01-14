bc8566d334841156147e4738b20a9534059dc5bb
WanQuanXie <i2cherry941219@gmail.com>
2024-01-15 01:18:05 +0800

    update: v2-进一步封装节点数据对象的创建过程

--- V01/main.js
+++ V01/main.js
@@ -11,23 +11,63 @@
 // container.appendChild(app);
 
 // V1 将需要创建的节点信息提取为独立的数据对象，节点创建和添加的写法不变，以旧使用原生写法
-const textEl = {
-    type: "TEXT_ELEMENT",
-    props: {
-        nodeValue: 'textNode',
+// const textEl = {
+//     type: "TEXT_ELEMENT",
+//     props: {
+//         nodeValue: 'textNode',
+//     }
+// }
+
+// const appEl = {
+//     type: 'div',
+//     props: {
+//         id: "app",
+//         children: [textEl]
+//     }
+// }
+
+// const app = document.createElement(appEl.type);
+// app.id = appEl.props.id;
+
+// appEl.props.children.forEach((el) => {
+//     const textNode = document.createTextNode(el.props.nodeValue);
+//     app.append(textNode);
+// })
+
+// document.getElementById('root').append(app);
+
+// V2 将节点的创建过程使用函数封装起来，以便能更动态化创建不同的节点
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children
+        }
     }
 }
 
-const appEl = {
-    type: 'div',
-    props: {
-        id: "app",
-        children: [textEl]
+function createTextElement(nodeValue, props) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            ...props,
+            nodeValue
+        }
     }
 }
+
+const textEl = createTextElement('textNode');
+
+const appEl = createElement('div', { id: 'app' }, textEl);
 
 const app = document.createElement(appEl.type);
-app.id = appEl.props.id;
+
+Object.keys(appEl.props).forEach(key => {
+    if (key !== 'children') {
+        app[key] = appEl[key];
+    }
+})
 
 appEl.props.children.forEach((el) => {
     const textNode = document.createTextNode(el.props.nodeValue);
